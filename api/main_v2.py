a1e1d5e010c442740b13e2c040b7b64bf228f886
Tonypost949 <tonypost949@users.noreply.github.com>
2026-08-15 09:28:51 -0700

    Initialize SQLite database on gunicorn import

--- api/main_v2.py
+++ api/main_v2.py
@@ -728,8 +728,9 @@ def init_db():
     conn.commit()
     conn.close()
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
 
